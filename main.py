bd35eed4991e59f7e8e3685d3fde8a1ac1953a8f
Evaldo Felipe <30991675+nicknamedelta@users.noreply.github.com>
2020-06-27 23:09:40 -0300

    2step - new arts, colors

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 import turtle
+from random import randrange
 from PIL import Image
+
+MAX_ANGLE = 30
+MAX_DISTANCE = 250
 
 # base colors
 colors = ['aqua', 'red', 'purple', 'blue', 'green', 'yellow', 'orange']
+colorss = ['gray', 'white']
 
 def rainbow_spiral():
     # background, speed and hide pen while draw
@@ -11,11 +16,14 @@ def rainbow_spiral():
     turtle.speed('fastest')
     turtle.hideturtle()
 
-    for x in range(360):
-        turtle.pencolor(colors[x%7])
+    for x in range(180):
+        # turtle.pencolor(colors[x%7])
+        turtle.pencolor(colorss[x%2])
         turtle.width(x/100+1)
         turtle.forward(x)
-        turtle.left(70)
+        turtle.right(80)
+
+    turtle.done()
         
 def rainbow_star():
     # background, speed and hide pen while draw
@@ -23,11 +31,71 @@ def rainbow_star():
     turtle.speed('fastest')
     turtle.hideturtle()
 
-    for x in range(360):
-        turtle.pencolor(colors[x%7])
+    for x in range(100):
+        # turtle.pencolor(colors[x%7])
+        turtle.pencolor(colorss[x%2])
         turtle.width(x/100+1)
         turtle.forward(x)
         turtle.left(170)
+
+    turtle.done()
+
+
+def rainbow_flower():
+    # background, speed and hide pen while draw
+    turtle.bgcolor('black')
+    turtle.speed('fastest')
+    turtle.hideturtle()
+
+    for angle in range(0, 360, 2):
+        turtle.penup()
+        turtle.home()
+        turtle.pendown()
+        turtle.setheading(angle)
+
+        while turtle.distance(0, 0) < MAX_DISTANCE:
+            # turtle.pencolor(colors[angle%7])
+            turtle.pencolor(colorss[angle%2])
+            angle = randrange(-MAX_ANGLE, MAX_ANGLE + 1)
+            turtle.right(angle)
+            turtle.forward(50)
+
+    turtle.done()
+
+def rainbow_leafs():
+    # background, speed and hide pen while draw
+    turtle.bgcolor('black')
+    turtle.speed('fastest')
+    turtle.hideturtle()
+
+    for x in range(100):
+        turtle.width(x/100+1)
+        for y in range(20):
+            # turtle.pencolor(colors[x%7])
+            turtle.pencolor(colorss[x%2])
+            turtle.forward(0.5+10+y)
+            turtle.left(77 / 9)
+        turtle.left(77)
+
+    turtle.done()
+
+def rainbow_unknown():
+    # background, speed and hide pen while draw
+    turtle.bgcolor('black')
+    turtle.speed('fastest')
+    turtle.hideturtle()
+
+    for x in range(180):
+        turtle.width(x/100+1)
+        for y in range(6):
+            # turtle.pencolor(colors[x%7])
+            turtle.pencolor(colorss[x%2])
+            turtle.forward(x/6)
+            turtle.left(40)
+        turtle.left(100)
+        # turtle.dot(6)
+
+    turtle.done()
 
 def save_as_png(canvas, fileName):
     # save postscipt image 
@@ -46,7 +114,7 @@ def save_as_png(canvas, fileName):
 
 def main():
     print('(｡^‿^｡) - "what i should draw today ?":\n')
-    print('1 - Spiral\n2 - Star')
+    print('1 - Spiral\n2 - Star\n3 - flower\n4 - leafs\n5 - unknown')
 
     ans = int(input())
 
@@ -56,6 +124,15 @@ def main():
     elif ans == 2:
         fileName = 'turtlepystar'
         rainbow_star()
+    elif ans == 3:
+        fileName = 'turtlepyflower'
+        rainbow_flower()
+    elif ans == 4:
+        fileName = 'turtlepyleafs'
+        rainbow_leafs()
+    elif ans == 5:
+        fileName = 'turtlepyunknown'
+        rainbow_unknown()
     else:
         print('(⋟﹏⋞) - "sorry, i can\'t make this."')
 
